c956368a23025b2ba211fc643a792422096996ee
kristeldev <kmanriquez.bettercommerce@gmail.com>
2025-04-09 23:15:22 -0400

    09-04 avances

---
+++ src/clases/Estudiante.java
@@ -0,0 +1,10 @@
+package clases;
+
+public class Estudiante extends Usuario {
+    private String carrera_cursando;
+
+    public Estudiante(String nombre_completo, String rut, char sexo, String carrera, String carrera_cursando) {
+        super(nombre_completo, rut, sexo, carrera);
+        this.carrera_cursando = carrera_cursando;
+    }
+}
--- src/utils/UtilsClass.java
+++ src/utils/UtilsClass.java
@@ -1,7 +1,6 @@
 package utils;
 
 public class UtilsClass {
-
     public String generarRespuestaGenerica() {
         return "Soy una respuesta generada";
     }
