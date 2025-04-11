401820de5470db55217f59336229a26e93d0f993
Carlos Muñoz Bustamante <carlos.a.munoz2@outlook.com>
2025-04-10 23:43:58 -0400

    Menu
    Crea usuarios
    borra usuarios
    lista usuarios actuales del mock

--- src/clases/Estudiante.java
+++ src/clases/Estudiante.java
@@ -7,4 +7,18 @@ public class Estudiante extends Usuario {
         super(nombre_completo, rut, sexo, carrera);
         this.carrera_cursando = carrera_cursando;
     }
+
+    public Estudiante(String carrera_cursando) {
+        this.carrera_cursando = carrera_cursando;
+    }
+
+
+    @Override
+    public Usuario crearUsuario(String nombre_completo, String rut, char sexo, String carrera) throws Exception {
+        this.setNombre_completo(nombre_completo);
+        this.setSexo(sexo);
+        this.setCarrera(carrera);
+        this.setRut(rut);
+        return Estudiante.this;
+    }
 }
